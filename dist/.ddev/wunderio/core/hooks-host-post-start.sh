b1fb93efe5fe7953244e054ea09729b6237b0fcb
Hannes Kirsman <hannes.kirsman@wunder.io>
2024-02-20 10:39:43 +0200

    #1 Color code the uli text.

--- dist/.ddev/wunderio/core/hooks-host-post-start.sh
+++ dist/.ddev/wunderio/core/hooks-host-post-start.sh
@@ -12,5 +12,10 @@ export PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/app/vendor/bin
 
 # Commands to run if Drupal is working.
 if ddev drush status bootstrap | grep -q 'Successful'; then
+    color_green="\033[38;5;70m"
+    color_reset="\033[0m"
+
+    printf "${color_green}Drupal is working, running drush uli: "
     ddev drush uli
+    printf "${color_reset}"
 fi
