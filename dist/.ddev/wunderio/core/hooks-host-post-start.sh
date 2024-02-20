3b4280b0293abb71f445c4b260064a3f1e0a22a4
Hannes Kirsman <hannes.kirsman@wunder.io>
2024-02-20 13:28:30 +0200

    #1 Support Drush 12 in hooks-host-post-start.sh.

--- dist/.ddev/wunderio/core/hooks-host-post-start.sh
+++ dist/.ddev/wunderio/core/hooks-host-post-start.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 #
 # Helper script to run host post-start commands.
@@ -10,8 +10,29 @@ if [ -n "${WUNDERIO_DEBUG:-}" ]; then
 fi
 export PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/app/vendor/bin
 
+# Function to check if Drupal is working.
+is_drupal_working() {
+    # Drush 11 and older.
+    output1=$(ddev drush status bootstrap 2>&1)
+    # Drush 12 and newer.
+    output2=$(ddev drush status --field=bootstrap 2>&1)
+    output="$output1$output2"
+
+    # Check if "Successful" is present in the combined output
+    if [[ $output == *"Successful"* ]]; then
+        # "Successful" found, Drupal is working.
+        echo 'success';
+        return 0
+    else
+        # "Successful" not found, Drupal is not working.
+        echo 'fail';
+        return 1
+    fi
+}
+
+
 # Commands to run if Drupal is working.
-if ddev drush status bootstrap | grep -q 'Successful'; then
+if is_drupal_working; then
     color_green="\033[38;5;70m"
     color_reset="\033[0m"
 
