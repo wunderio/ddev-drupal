17306fd09806204243e54cbb88427612b6e79171
Hannes Kirsman <hannes.kirsman@wunder.io>
2024-02-20 10:26:56 +0200

    #1 Remove 'ddev status' from post start script to have leaner output.

--- dist/.ddev/wunderio/core/hooks-host-post-start.sh
+++ dist/.ddev/wunderio/core/hooks-host-post-start.sh
@@ -10,8 +10,6 @@ if [ -n "${WUNDERIO_DEBUG:-}" ]; then
 fi
 export PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/app/vendor/bin
 
-ddev status
-
 # Commands to run if Drupal is working.
 if ddev drush status bootstrap | grep -q 'Successful'; then
     ddev drush uli
