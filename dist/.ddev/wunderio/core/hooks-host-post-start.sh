20fb54c6602ea36d9c1b31cc431535ed34b7e0e4
Hannes Kirsman <hannes.kirsman@wunder.io>
2024-02-21 08:42:54 +0200

    #1 Fix DDEV hanging when running is_drupal_working.

--- dist/.ddev/wunderio/core/hooks-host-post-start.sh
+++ dist/.ddev/wunderio/core/hooks-host-post-start.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 #
 # Helper script to run host post-start commands.
@@ -8,32 +8,4 @@ set -eu
 if [ -n "${WUNDERIO_DEBUG:-}" ]; then
     set -x
 fi
-export PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/app/vendor/bin
-
-# Function to check if Drupal is working.
-is_drupal_working() {
-    # Drush 11 and older.
-    output1=$(ddev drush status bootstrap 2>&1)
-    # Drush 12 and newer.
-    output2=$(ddev drush status --field=bootstrap 2>&1)
-    output="$output1$output2"
-
-    # Check if "Successful" is present in the combined output
-    if [[ $output == *"Successful"* ]]; then
-        # "Successful" found, Drupal is working.
-        return 0
-    else
-        # "Successful" not found, Drupal is not working.
-        return 1
-    fi
-}
-
-# Commands to run if Drupal is working.
-if is_drupal_working; then
-    color_green="\033[38;5;70m"
-    color_reset="\033[0m"
-
-    printf "${color_green}Drupal is working, running drush uli: "
-    ddev drush uli
-    printf "${color_reset}"
-fi
+export PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin
--- dist/.ddev/wunderio/core/hooks-web-post-start.sh
+++ dist/.ddev/wunderio/core/hooks-web-post-start.sh
@@ -8,4 +8,33 @@ set -eu
 if [ -n "${WUNDERIO_DEBUG:-}" ]; then
     set -x
 fi
-export PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/app/vendor/bin
+export PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/var/www/html/vendor/bin
+
+# Function to check if Drupal is working.
+is_drupal_working() {
+    # Drush 11 and older.
+    status=$(drush status bootstrap 2>&1)
+    if echo "$status" | grep -q "Successful"; then
+        # "Successful" found, Drupal is working.
+        return 0
+    fi
+
+    # Drush 12 and newer.
+    status=$(drush status --field=bootstrap 2>&1)
+    if echo "$status" | grep -q "Successful"; then
+        # "Successful" found, Drupal is working.
+        return 0
+    fi
+
+    return 1
+}
+
+# Commands to run if Drupal is working.
+if is_drupal_working; then
+    color_green="\033[38;5;70m"
+    color_reset="\033[0m"
+
+    printf "${color_green}Drupal is working, running drush uli: "
+    drush uli
+    printf "${color_reset}"
+fi
