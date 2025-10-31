8bd0ac1ebf73389eb29dac92831765595f3ff65a
Santeri Kinnunen <santeri.kinnunen@wunder.io>
2025-10-31 14:14:12 +0200

    Add Codeception binary check before command is ran

--- dist/.ddev/commands/web/wunderio-core-codeception.sh
+++ dist/.ddev/commands/web/wunderio-core-codeception.sh
@@ -4,4 +4,9 @@
 ## Usage: codecept
 ## Example: "ddev codecept"
 
+if [ ! -f "$DDEV_COMPOSER_ROOT/vendor/bin/codecept" ]; then
+  echo "Composer binaries for Codecept missing; exiting early."
+  exit 0
+fi
+
 $DDEV_COMPOSER_ROOT/vendor/bin/codecept "$@"
