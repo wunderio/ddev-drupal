3d8e698eb4160f3d1f9dce17c1c29c4977df2d3d
Santeri Kinnunen <santeri.kinnunen@wunder.io>
2025-11-03 11:23:40 +0200

    Correct command

--- dist/.ddev/wunderio/core/_helpers.sh
+++ dist/.ddev/wunderio/core/_helpers.sh
@@ -18,7 +18,7 @@ display_status_message() {
     printf "${color_green}${message}${color_reset}\n"
 }
 
-# Function to display status message
+# Function to display error message
 display_error_message() {
     local color_red="\033[0;31m"
     local color_reset="\033[0m"
